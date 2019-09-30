f58731058b9975064b989811aac25782eede08b3
Grzegorz on Mac <grzegorz.zajac@me.com>
2019-09-30 21:32:17 +0200

    add sys info

--- fs/init.js
+++ fs/init.js
@@ -1,11 +1,13 @@
 // Load Mongoose OS API
 load('api_timer.js');
 load('api_dht.js');
+load('api_sys.js');
+load('api_mqtt.js');
 
 // GPIO pin which has a DHT sensor data wire connected
 let pin = 16;
-let ver = 1.0;
-
+let ver = 1.1;
+let state = {on: false};
 // Initialize DHT library
 let dht = DHT.create(pin, DHT.DHT22);
 
@@ -13,13 +15,16 @@ let dht = DHT.create(pin, DHT.DHT22);
 Timer.set(2000 /* milliseconds */, Timer.REPEAT, function() {
   let t = dht.getTemp();
   let h = dht.getHumidity();
-
   if (isNaN(h) || isNaN(t)) {
     print('Failed to read data from sensor');
     return;
   }
+  print('Temperature:', t, '*C','  Humidity:', h, '%','  Version: ' ver);
+}, null);
 
-  print('Temperature:', t, '*C');
-  print('Humidity:', h, '%');
-  print('Version: ' ver);
+
+Timer.set(5000, Timer.REPEAT, function() {
+  state.uptime = Sys.uptime();
+  state.ram_free = Sys.free_ram();
+  print("syestem: ", JSON.stringify(state));
 }, null);
