6f75e589ee2c2656fc037869f214b4bf12b7152d
Grzegorz on Mac <grzegorz.zajac@me.com>
2019-10-01 15:59:57 +0200

    basic dht, striped down ver, prep for oled

--- fs/init.js
+++ fs/init.js
@@ -4,29 +4,31 @@ load('api_dht.js');
 load('api_sys.js');
 load('api_mqtt.js');
 load('api_config.js');
-load('api_arduino_ssd1306.js');
+// load('api_arduino_ssd1306.js');
 
 // Initialize Adafruit_SSD1306 library (I2C)
-let d = Adafruit_SSD1306.create_i2c(2 /* RST GPIO */, Adafruit_SSD1306.RES_128_64);
+
+//let d = Adafruit_SSD1306.create_i2c(2 /* RST GPIO */, Adafruit_SSD1306.RES_128_64);
 // Initialize the display. 0x78 myoled for test, external
-d.begin(Adafruit_SSD1306.SWITCHCAPVCC, 0x78, true /* reset */);
-d.display();
+
+//d.begin(Adafruit_SSD1306.SWITCHCAPVCC, 0x78, true /* reset */);
+//d.display();
 let i = 0;
 
-let showStr = function(d, str) {
-  d.clearDisplay();
-  d.setTextSize(2);
-  d.setTextColor(Adafruit_SSD1306.WHITE);
-  d.setCursor(d.width() / 4, d.height() / 4);
-  d.write(str);
-  d.display();
-};
-
-Timer.set(1000 /* milliseconds */, Timer.REPEAT, function() {
-  showStr(d, "i = " + JSON.stringify(i));
-  print("i = ", i);
-  i++;
-}, null);
+// let showStr = function(d, str) {
+//   d.clearDisplay();
+//   d.setTextSize(2);
+//   d.setTextColor(Adafruit_SSD1306.WHITE);
+//   d.setCursor(d.width() / 4, d.height() / 4);
+//   d.write(str);
+//   d.display();
+// };
+//
+// Timer.set(1000 /* milliseconds */, Timer.REPEAT, function() {
+//   showStr(d, "i = " + JSON.stringify(i));
+//   print("i = ", i);
+//   i++;
+// }, null);
 
 // GPIO pin which has a DHT sensor data wire connected
 let pin = 16;
@@ -34,10 +36,11 @@ let ver = 1.83;
 let sensor = {working: false};
 // Initialize DHT library
 let dht = DHT.create(pin, DHT.DHT22);
-let topic = 'rack/homekit/#';
-MQTT.sub(topic, function(conn, topic, msg) {
-  print('Topic:', topic, 'message:', msg);
-}, null);
+
+// let topic = 'rack/homekit/#';
+// MQTT.sub(topic, function(conn, topic, msg) {
+//   print('Topic:', topic, 'message:', msg);
+// }, null);
 
 // This function reads data from the DHT sensor every 2 second
 Timer.set(10000 /* milliseconds */, Timer.REPEAT, function() {
@@ -53,8 +56,8 @@ Timer.set(10000 /* milliseconds */, Timer.REPEAT, function() {
   }
   // TODO send sensor.working message even if sensor disconected
   print('Temperature:', sensor.temperature, '*C','  Humidity:', sensor.humidity, '%','  Version: ' sensor.version);
-  let message = JSON.stringify(sensor);
-  let topic = "mongoose/sensor";
-  let ok = MQTT.pub(topic, message, 1);
-  print('Published:', ok ? 'yes' : 'no', 'topic:', topic, 'message:', message);
+  // let message = JSON.stringify(sensor);
+  // let topic = "mongoose/sensor";
+  // let ok = MQTT.pub(topic, message, 1);
+  // print('Published:', ok ? 'yes' : 'no', 'topic:', topic, 'message:', message);
 }, null);
